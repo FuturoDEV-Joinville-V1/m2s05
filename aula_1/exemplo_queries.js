553bc8e06cbc74fff2b0a16f7e7c618e95abcadf
Your Name <you@example.com>
2025-06-16 20:46:40 -0300

    [A01] Exemplo queries

---
+++ aula_1/exemplo_queries.js
@@ -0,0 +1,16 @@
+
+const titulo = document.getElementById('titulo');
+
+console.log('titulo:', titulo);
+
+const tag = document.getElementsByTagName('p');
+
+console.log('tag:', tag);
+
+const paragrafo = document.getElementsByClassName('paragrafo');
+
+console.log('paragrafo:', paragrafo);
+
+const selectorParagrafo = document.querySelectorAll('p.paragrafo');
+
+console.log('selectorParagrafo:', selectorParagrafo);
